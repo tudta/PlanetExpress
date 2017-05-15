3dfda1a91e18cbbfc96d1f23506e8bcf70b90891
Unknown <michaelphillipsv@gmail.com>
2017-05-14 17:30:37 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/dev' into phillips""
    This reverts commit bdf3f6de136d79da8a12021e1edaaf084072f178.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager instance = null;
+    [SerializeField] private GameStates currentState = GameStates.PLAY;
     private string buildingPath = "Prefabs/";
+    private Player player = null;
 
-	// Use this for initialization
-	void Start () {
-	
+    public static GameManager Instance {get{return instance;} set{instance = value;}}
+    public GameStates CurrentState {get{return currentState;} set{currentState = value;}}
+    
+    void Awake() {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        player = Player.Instance;
 	}
 	
 	// Update is called once per frame
@@ -15,11 +25,12 @@ public class GameManager : MonoBehaviour
 	
 	}
 
-    public void CreateBuilding(string name)
-    {
+    public void CreateBuilding(string name) {
         GameObject tmpGO = (GameObject)Resources.Load(buildingPath + name);
         tmpGO = Instantiate(tmpGO);
-        Player.Instance.BuildingObj = tmpGO;
-        Player.Instance.SwitchState("BUILD");
+        Building tmpBuilding = tmpGO.GetComponent<Building>();
+        player.TarBuildingObj = tmpGO;
+        player.TarBuilding = tmpBuilding;
+        player.SwitchState("BUILD");
     }
 }
